b4eeab9093467563df7779dc392fc7fe274ae519
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-08-11 14:00:50 +0900

    準備完了

--- start_lambda.sh
+++ start_lambda.sh
@@ -4,16 +4,5 @@ export AWS_LAMBDA=1
 export DOTNET_EnableWriteXorExecute=0
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-# time ./main <in.txt >out.txt 2>err.txt
-# time ./tester ./main <in.txt >out.txt
-# ./vis in.txt out.txt
-
-./main
-
-cat <<EOS
-Score	1234567890 pt
-Rate:50.24%
-[WA]
-
-real    0m1.002s
-EOS
+time ./tester ./main <in.txt >out.txt || exit 0
+./vis in.txt out.txt || exit 0
